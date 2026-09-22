352af2eab1a7ad5256bc9d8dadbb615cc99c331d
Дягилев Дмитрий Викторович <10231355@college.local>
2026-09-22 20:10:43 +0300

    Сделали форму регистрации

---
+++ demic_solution/demic_solution/AppFrame.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace demic_solution
+{
+    public class AppFrame
+    {
+        public static Frame mainFrame;
+    }
+}
